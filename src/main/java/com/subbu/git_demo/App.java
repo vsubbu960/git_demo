1e1cde47244296653836afdbe934462f332d9d20
vsubbu960 <gvsubbaiah960@gmail.com>
2018-12-21 22:15:41 +0530

    added git_demo project to local repo

---
+++ src/main/java/com/subbu/git_demo/App.java
@@ -0,0 +1,17 @@
+package com.subbu.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void addData(String data) {
+		System.out.println("data..."+data);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
